3e94bcdc4e9bb9557c019d26ada826b85ce7f43b
lprivette <lprivette@usgs.gov>
2021-06-08 17:31:31 -0400

    fixes error in tooltip test

--- assets/test/js/portal_ui/tooltipSpec.js
+++ assets/test/js/portal_ui/tooltipSpec.js
@@ -2,12 +2,11 @@ import { initTooltip } from '../../../js/uswdsComponents/uswdsTooltip';
 
 describe('Creates a tooltip', function() {
     var testDiv = document.createElement('div');
-    testDiv.setAttribute('id', 'test-div')
-    testDiv.setAttribute('title', 'Tooltip test');;
+    $(testDiv).attr('class', 'test-div')
 
     it('Creates a tooltip widget', () => {
-        let tooltipItem;f
-        initTooltip(testDiv);
+        let tooltipItem;
+        initTooltip($(testDiv));
         tooltipItem = testDiv.className;
         expect(tooltipItem).toContain('usa-tooltip');
     });
